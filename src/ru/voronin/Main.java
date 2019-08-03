66d721ebf7b4eda39b46be87baee2c88979d70ee
VoroninS <javaiirush@gmail.com>
2019-08-03 16:04:31 +0500

    Hi max

--- src/ru/voronin/Main.java
+++ src/ru/voronin/Main.java
@@ -3,6 +3,7 @@ package ru.voronin;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Hi my brother Max");
+
     }
 }
